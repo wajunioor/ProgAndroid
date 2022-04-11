bfe0800d7701724905900bccb40538bf85145aec
wajunioor <juniorwashigton@hotmail.com>
2022-04-11 20:47:03 -0300

    jr

--- GestaoBancaria/home/App.js
+++ GestaoBancaria/home/App.js
@@ -1,22 +1,46 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, TextInput, TouchableOpacity, StyleSheet} from 'react-native';
 import {GetInput} from '../components/Inputs';
 
 const TelaInicial = () => {
     return (
-        <View style={{
-            flexDirection: 'row',
-            padding: 20,
-            backgroundColor: '#000'
-        }}>
-            <Text style={{
-                fontSize: 35,
-                fontWeight: '600',
-                color: '#FFF'
-            }}>Oi carai</Text>
-            <GetInput></GetInput>
+        <View style={style.background}>
+           
+        
+            <Image 
+                source={require('../imagens/will.jpg')}
+                style={{width:'100%'}}
+                resizeMode= 'center'
+                
+            
+            />
+
+            <TextInput
+                style={style.input}
+                placeholder='Usuario'
+                onChangeText={() => {}}
+            ></TextInput>
+
+            <TextInput
+                style={style.input}
+                placeholder='Senha'
+                autoComplete='false'
+                onChangeText={() =>{}}
+            ></TextInput>
         </View>
     );
 }
 
+const style = StyleSheet.create({
+    background:{
+        flex:1,
+        justifyContent: 'center',
+        aliggItems: 'center',
+        backgroundColor: '#191919'
+    },
+    input:{
+        flex:1,
+        justifyContent: 'center'
+    }
+});
 export default TelaInicial;
